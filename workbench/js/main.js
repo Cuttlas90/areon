53b78436d086dd0016c357556ef5714c64ed700c
Cuttlas <mohsenbarekati@gmail.com>
2024-04-15 15:38:37 +0330

    re-structure, every part will deploy seperatly

--- workbench/js/main.js
+++
@@ -1,8 +0,0 @@
-
-var baseUrl = 'https://areon-api.cuttlas.app';
-var isAdmin = false;
-
-function numberWithCommas(x) {
-	//return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-	return x.toLocaleString();
-}
